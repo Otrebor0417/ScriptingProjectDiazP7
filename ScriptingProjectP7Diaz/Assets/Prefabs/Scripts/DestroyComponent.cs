8d2843f36741ea46287d4285c6d739d93a1c06e6
Otrebor0417 <roborobert185@gmail.com>
2024-02-14 14:43:40 -0800

    Made a scene and 3 scripts
    Made a new scene and 3 new scripts that destroyed a cube.

---
+++ ScriptingProjectP7Diaz/Assets/Prefabs/Scripts/DestroyComponent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroyComponent : MonoBehaviour
+{
+   void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+
+            Destroy(GetComponent<MeshRenderer>());
+        }        
+    }
+}
---
+++ ScriptingProjectP7Diaz/Assets/Prefabs/Scripts/DestroyOther.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+
+
+ 
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }        
+    }
+}
